a31e30ab5d2ff7d2605568d2081b9df51a65fe17
francogonzalezorellano <fgonzalezorellano@est.frba.utn.edu.ar>
2020-08-26 11:43:18 -0300

    Emprolijo

--- .next/static/development/pages/_app.js
+++ .next/static/development/pages/_app.js
@@ -359,7 +359,7 @@ module.exports = __webpack_require__(/*! regenerator-runtime */ "./node_modules/
 
 exports = module.exports = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js")(true);
 // Module
-exports.push([module.i, "* {\n  margin: 0px;\n  padding: 0px;\n}\n\n:root {\n    --color-background: #1b1b1b;\n    --color-black: #000000;\n    --color-grey: #222222;\n    --color-white: #ffffff;\n    --color-pallete1: #180233;\n    --color-pallete2: #200a48;\n    --color-pallete3: #2d1753;\n    --color-pallete4: #2d1753;\n    \n    --nav-size: 60px;\n    --border-radius: 8px;\n    --speed:500ms;\n\n  }\n\nhtml,\nbody {\n  font-family: 'Montserrat';\n  font-weight: regular;\n  background-color: var(--color-background);\n}\n\nul{\n  list-style: none;\n  margin: 0;\n  padding: 0;\n}\n\na {\n  color: var(--text-color);\n  text-decoration: none;\n}", "",{"version":3,"sources":["/Users/francogonzalez/Documents/Trabajo/my-landing-page/styles/global.css"],"names":[],"mappings":"AAAA;EACE,WAAW;EACX,YAAY;AACd;;AAEA;IACI,2BAA2B;IAC3B,sBAAsB;IACtB,qBAAqB;IACrB,sBAAsB;IACtB,yBAAyB;IACzB,yBAAyB;IACzB,yBAAyB;IACzB,yBAAyB;;IAEzB,gBAAgB;IAChB,oBAAoB;IACpB,aAAa;;EAEf;;AAEF;;EAEE,yBAAyB;EACzB,oBAAoB;EACpB,yCAAyC;AAC3C;;AAEA;EACE,gBAAgB;EAChB,SAAS;EACT,UAAU;AACZ;;AAEA;EACE,wBAAwB;EACxB,qBAAqB;AACvB","file":"global.css","sourcesContent":["* {\n  margin: 0px;\n  padding: 0px;\n}\n\n:root {\n    --color-background: #1b1b1b;\n    --color-black: #000000;\n    --color-grey: #222222;\n    --color-white: #ffffff;\n    --color-pallete1: #180233;\n    --color-pallete2: #200a48;\n    --color-pallete3: #2d1753;\n    --color-pallete4: #2d1753;\n    \n    --nav-size: 60px;\n    --border-radius: 8px;\n    --speed:500ms;\n\n  }\n\nhtml,\nbody {\n  font-family: 'Montserrat';\n  font-weight: regular;\n  background-color: var(--color-background);\n}\n\nul{\n  list-style: none;\n  margin: 0;\n  padding: 0;\n}\n\na {\n  color: var(--text-color);\n  text-decoration: none;\n}"]}]);
+exports.push([module.i, "* {\n  margin: 0px;\n  padding: 0px;\n}\n\n:root {\n    --color-background: #1b1b1b;\n    --color-black: #000000;\n    --color-grey: #222222;\n    --color-white: #ffffff;\n    --color-pallete1: #180233;\n    --color-pallete2: #200a48;\n    --color-pallete3: #2d1753;\n    --color-pallete4: #2d1753;\n    \n    --nav-size: 60px;\n    --border-radius: 8px;\n    --speed:500ms;\n\n  }\n\nhtml,\nbody {\n  font-family: 'Montserrat';\n  font-weight: regular;\n  background-color: var(--color-background);\n}\n\nul{\n  list-style: none;\n  margin: 0;\n  padding: 0;\n}\n\na {\n  color: var(--text-color);\n  text-decoration: none;\n}", "",{"version":3,"sources":["/Users/francogonzalez/Documents/Trabajo/franco-gonzalez-landing-page/styles/global.css"],"names":[],"mappings":"AAAA;EACE,WAAW;EACX,YAAY;AACd;;AAEA;IACI,2BAA2B;IAC3B,sBAAsB;IACtB,qBAAqB;IACrB,sBAAsB;IACtB,yBAAyB;IACzB,yBAAyB;IACzB,yBAAyB;IACzB,yBAAyB;;IAEzB,gBAAgB;IAChB,oBAAoB;IACpB,aAAa;;EAEf;;AAEF;;EAEE,yBAAyB;EACzB,oBAAoB;EACpB,yCAAyC;AAC3C;;AAEA;EACE,gBAAgB;EAChB,SAAS;EACT,UAAU;AACZ;;AAEA;EACE,wBAAwB;EACxB,qBAAqB;AACvB","file":"global.css","sourcesContent":["* {\n  margin: 0px;\n  padding: 0px;\n}\n\n:root {\n    --color-background: #1b1b1b;\n    --color-black: #000000;\n    --color-grey: #222222;\n    --color-white: #ffffff;\n    --color-pallete1: #180233;\n    --color-pallete2: #200a48;\n    --color-pallete3: #2d1753;\n    --color-pallete4: #2d1753;\n    \n    --nav-size: 60px;\n    --border-radius: 8px;\n    --speed:500ms;\n\n  }\n\nhtml,\nbody {\n  font-family: 'Montserrat';\n  font-weight: regular;\n  background-color: var(--color-background);\n}\n\nul{\n  list-style: none;\n  margin: 0;\n  padding: 0;\n}\n\na {\n  color: var(--text-color);\n  text-decoration: none;\n}"]}]);
 
 
 /***/ }),
@@ -3455,7 +3455,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _styles_global_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../styles/global.css */ "./styles/global.css");
 /* harmony import */ var _styles_global_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_styles_global_css__WEBPACK_IMPORTED_MODULE_2__);
 
-var _jsxFileName = "/Users/francogonzalez/Documents/Trabajo/my-landing-page/pages/_app.js";
+var _jsxFileName = "/Users/francogonzalez/Documents/Trabajo/franco-gonzalez-landing-page/pages/_app.js";
 
 var __jsx = react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement;
 
@@ -3543,7 +3543,7 @@ if (true) {
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! next-client-pages-loader?page=%2F_app&absolutePagePath=private-next-pages%2F_app.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_app&absolutePagePath=private-next-pages%2F_app.js!./");
-module.exports = __webpack_require__(/*! /Users/francogonzalez/Documents/Trabajo/my-landing-page/node_modules/next/dist/client/router.js */"./node_modules/next/dist/client/router.js");
+module.exports = __webpack_require__(/*! /Users/francogonzalez/Documents/Trabajo/franco-gonzalez-landing-page/node_modules/next/dist/client/router.js */"./node_modules/next/dist/client/router.js");
 
 
 /***/ }),
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d83f94088dea3261aaaa";
+/******/ 	var hotCurrentHash = "174766f14f2172f20910";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
